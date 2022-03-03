873b4f47dcdc7167748d395d0e250f4ed6e022e6
Leeseunghwan7305 <sun1238799@naver.com>
2022-03-03 10:20:40 +0900

    redux Habit

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -1,7 +1,37 @@
 import React from "react";
-
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../modules/index";
+import { decrease } from "../../modules/todos";
+import styles from "./body.module.css";
+import { toggle } from "../../modules/todos";
 const Body = () => {
-  return <div>몸</div>;
+  const todos = useSelector((state: RootState) => state.todos);
+  const dispatch = useDispatch();
+  return (
+    <div className={styles.body}>
+      {todos.map((item, i) => {
+        return (
+          <div key={i}>
+            <p
+              className={item.check ? styles.check : ""}
+              onClick={() => {
+                dispatch(toggle(item.id));
+              }}
+            >
+              {item.text}
+            </p>
+            <button
+              onClick={() => {
+                dispatch(decrease(item.id));
+              }}
+            >
+              삭제하기
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Body;
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useDispatch } from "react-redux";
+import { increase } from "../../modules/todos";
+import styles from "./header.module.css";
 const Header = () => {
-  return <div>머리</div>;
+  let dispatch = useDispatch();
+  let [input, setInput] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+  return (
+    <div className={styles.head}>
+      <h1>HabitTracker</h1>
+      <div>
+        <input
+          ref={inputRef}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setInput(e.target.value);
+          }}
+          placeholder="내용을 입력해주세요"
+        ></input>
+        <button
+          onClick={() => {
+            dispatch(increase(input));
+            setInput("");
+            inputRef.current?.focus();
+            if (inputRef.current) {
+              inputRef.current.value = "";
+            }
+          }}
+        >
+          추가하기
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
